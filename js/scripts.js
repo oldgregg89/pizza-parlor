625845264112d8922f0efeee40295d1c4559feeb
Ian Gregg <iangregg188@gmail.com>
2020-06-28 21:00:16 -0700

    updated business logicand UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,45 @@
 //Business Logic
 
-function Pizza(size, sauce, toppings){
-  this.size = size
-  this.sauce = sauce
-  this.toppings = toppings
-  this.price = 0
+function Pizza(){
+  this.price = 0;
 }
 
-Pizza.prototype.getSize = function() {
-  this.size = $("input[type='radio'][name='size']:checked").attr("id")
-    if (this.size === "small"){
-      this.price += 10.00
-    } else if (this.size === "medium") {
-      this.price += 12.00
+Pizza.prototype.getSize = function(size) {
+    if (size === "small"){
+      this.price += 10.00;
+    } else if (size === "medium") {
+      this.price += 12.00;
     } else {
-      this.price += 15.00
-    }
-
-}
-
-Pizza.prototype.getSauce =function() {
-  this.sauce = $("input[type='radio'][name='sauce']:checked").attr("id")
-    if (this.sauce === "pesto"){
-      this.price += 3.00
-    } else if (this.sauce === "alfredo") {
-      this.price += 2.00
-    } else {
-      this.price += .50
+      this.price += 15.00;
     }
 }
 
-Pizza.prototype.checkToppings = function() {
-  this.toppings = $(".toppings:checked").length
-  this.price += this.toppings
+Pizza.prototype.getSauce =function(sauce) {
+    if (sauce === "pesto"){
+      this.price += 3.00;
+    } else if (sauce === "alfredo") {
+      this.price += 2.00;
+    } else {
+      this.price += .50;
+    }
+}
 
+Pizza.prototype.checkToppings = function(toppings) {
+  this.price += toppings;
 }
 
 //User interface logic
 $(document).ready(function(){
   $("form").submit(function(event){
+    let pizza = new Pizza();
+    let size = $("input[type='radio'][name='size']:checked").attr("id");
+    let sauce = $("input[type='radio'][name='sauce']:checked").attr("id");
+    let toppings = $(".toppings:checked").length;
     event.preventDefault(); 
-    let pizza = new Pizza('','','')
-    pizza.getSize()
-    pizza.getSauce()
-    pizza.checkToppings()
-    let total = pizza.price
-    debugger
-    $("#priceOfPizza").text('$'+total)
+    pizza.getSize(size);
+    pizza.getSauce(sauce);
+    pizza.checkToppings(toppings);
+    let total = pizza.price;
+    $("#priceOfPizza").text('$'+total);
   });
 });
